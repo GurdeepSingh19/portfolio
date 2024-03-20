bce9febf35c3d1b633cd17f481471c3607de08fb
gurdeep <gurdeepnz19@gmail.com>
2024-03-21 02:52:45 +1300

    Change project Image

--- src/Content.js
+++ src/Content.js
@@ -15,7 +15,6 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img3.png";
 import project2 from "./assets/images/projects/img2.png";
 import project3 from "./assets/images/projects/img3.png";
 import person_project from "./assets/images/projects/person.png";
@@ -157,7 +156,7 @@ export const content = {
     project_content: [
       {
         title: "Gym Website",
-        image: project1,
+        image: "",
       },
       {
         title: "Social Media web",
